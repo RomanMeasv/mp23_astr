4c636335e1854ccbc088fe2bb90c96e815eeeab9
RomanMeasv <90683512+RomanMeasv@users.noreply.github.com>
2023-04-20 01:29:35 +0200

    removing items

--- lib/models/list_entry.dart
+++ lib/models/list_entry.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 class ListEntry {
   String text;
   dynamic image;
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,30 +4,6 @@ import 'package:mp23_astr/widgets/carousel_card.dart';
 import '../models/list_entry.dart';
 
 class Home extends StatefulWidget {
-  // In Flutter it complains when you use the new keyword??
-  final List<ListEntry> listEntries = [
-    ListEntry(
-      text: 'Item1',
-      image: const NetworkImage('https://picsum.photos/id/1/500'),
-    ),
-    ListEntry(
-      text: 'Item2',
-      image: const NetworkImage('https://picsum.photos/id/2/500'),
-    ),
-    ListEntry(
-      text: 'Item3',
-      image: const NetworkImage('https://picsum.photos/id/3/500'),
-    ),
-    ListEntry(
-      text: 'Item4',
-      image: const NetworkImage('https://picsum.photos/id/4/500'),
-    ),
-    ListEntry(
-      text: 'Item5',
-      image: const NetworkImage('https://picsum.photos/id/5/500'),
-    ),
-  ];
-
   Home({super.key});
 
   @override
@@ -35,19 +11,57 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  // In Flutter it complains when you use the new keyword??
+  final List<ListEntry> listEntries = [
+    ListEntry(
+      text: 'Item1',
+      image: const NetworkImage('https://picsum.photos/id/1/1920'),
+    ),
+    ListEntry(
+      text: 'Item2',
+      image: const NetworkImage('https://picsum.photos/id/2/1920'),
+    ),
+    ListEntry(
+      text: 'Item3',
+      image: const NetworkImage('https://picsum.photos/id/3/1920'),
+    ),
+    ListEntry(
+      text: 'Item4',
+      image: const NetworkImage('https://picsum.photos/id/4/1920'),
+    ),
+    ListEntry(
+      text: 'Item5',
+      image: const NetworkImage('https://picsum.photos/id/5/1920'),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    final PageController controller = PageController(initialPage: 1);
+    final PageController controller = PageController();
 
     return Center(
       child: PageView.builder(
         controller: controller,
-        itemCount: widget.listEntries.length,
+        itemCount: listEntries.length,
         itemBuilder: (context, index) {
-          ListEntry listEntry = widget.listEntries[index];
-          return CarouselCard(entry: listEntry);
+          final ListEntry listEntry = listEntries[index];
+          return Dismissible(
+            background: Container(
+              color: Colors.red,
+            ),
+            direction: DismissDirection.up,
+            key: ObjectKey(listEntry),
+            onDismissed: (_) => onEntryDismissed(index),
+            child: CarouselCard(entry: listEntry),
+          );
         },
       ),
     );
+  }
+
+  void onEntryDismissed(index) {
+    setState(() {
+      listEntries.removeAt(index);
+    });
   }
 }
