138f34cbbb64c27cbe01647932499f98c031a4eb
Sergio M <serg0971@easv365.dk>
2023-05-10 12:56:36 +0200

    Fonts set up

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -2,14 +2,30 @@ import 'package:flutter/material.dart';
 
 final ThemeData appThemeData = ThemeData(
   primaryColor: Colors.blueAccent,
-  fontFamily: 'Georgia',
+  fontFamily: 'Nunito',
   textTheme: const TextTheme(
-    titleLarge: TextStyle(fontSize: 40.0),
-    displayLarge: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w900),
-    displayMedium: TextStyle(fontSize: 24.0),
-    displaySmall: TextStyle(fontSize: 18.0),
-    bodyLarge: TextStyle(fontSize: 14.0),
-  ),
+
+    titleLarge: TextStyle(fontSize: 60.0, fontWeight: FontWeight.bold),
+    titleMedium: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+    titleSmall: TextStyle(fontSize: 12.0, fontWeight: FontWeight.bold),
+
+    displayLarge: TextStyle(fontSize: 20.0),
+    displayMedium: TextStyle(fontSize: 16.0),
+    displaySmall: TextStyle(fontSize: 12.0),
+
+    bodyLarge: TextStyle(fontSize: 20.0),
+    bodyMedium: TextStyle(fontSize: 16.0),
+    bodySmall: TextStyle(fontSize: 12.0),
+
+    labelLarge: TextStyle(fontSize: 20.0),
+    labelMedium: TextStyle(fontSize: 16.0),
+    labelSmall: TextStyle(fontSize: 12.0),
+
+    headlineLarge: TextStyle(fontSize: 20.0),
+    headlineMedium: TextStyle(fontSize: 16.0),
+    headlineSmall: TextStyle(fontSize: 12.0),
+    
+    ),
   colorScheme: const ColorScheme(
       primary: Color(0xffE2711D),
       secondary: Color(0xff800020),
