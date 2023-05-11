058cf6ccdb9ed420ae27a0aaa9e9751a616bcf89
Sergio M <serg0971@easv365.dk>
2023-05-11 15:15:26 +0200

    Finished styling Register page
    starting with validation. Oh boi

--- lib/app/modules/user_module/widgets/register_widget.dart
+++ lib/app/modules/user_module/widgets/register_widget.dart
@@ -10,13 +10,17 @@ class RegisterWidget extends StatefulWidget {
 
 class _RegisterWidgetState extends State<RegisterWidget> {
 
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool _obscureTextRepPass = true;
+  bool _obscureTextPass = true;
+  final UserController controller = Get.find();
 
   @override
   Widget build(BuildContext context) {
-    final UserController controller = Get.find();
+
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -28,7 +32,128 @@ class _RegisterWidgetState extends State<RegisterWidget> {
                 color: Theme.of(context).colorScheme.primary,
               )),
         const Padding(padding: EdgeInsets.only(top: 30)),
-        
+        Container(
+          margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+          child: TextField(
+            controller: _nameController,
+            decoration: InputDecoration(
+              labelText: 'Username',
+              hintText: 'example@gmail.com',
+              prefixIcon: const Icon(Icons.email),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5),
+                borderSide: BorderSide(
+                  color: Theme.of(context).colorScheme.primary,
+                  width: 2.0,
+                ),
+              ),
+              filled: true,
+              fillColor: Colors.grey[100],
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+          child: TextField(
+            controller: _emailController,
+            decoration: InputDecoration(
+              labelText: 'Email',
+              hintText: 'example@gmail.com',
+              prefixIcon: const Icon(Icons.email),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5),
+                borderSide: BorderSide(
+                  color: Theme.of(context).colorScheme.primary,
+                  width: 2.0,
+                ),
+              ),
+              filled: true,
+              fillColor: Colors.grey[100],
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+          child: StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+              return TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: '••••••••',
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureTextPass ? Icons.visibility_off : Icons.visibility,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureTextPass = !_obscureTextPass;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).colorScheme.primary,
+                      width: 2.0,
+                    ),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[100],
+                ),
+                obscureText: _obscureTextPass,
+              );
+            },
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+          child: StatefulBuilder(
+            builder: (BuildContext context, StateSetter setState) {
+              return TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Repeat password',
+                  hintText: '••••••••',
+                  prefixIcon: Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscureTextRepPass ? Icons.visibility_off : Icons.visibility,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureTextRepPass = !_obscureTextRepPass;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).colorScheme.primary,
+                      width: 2.0,
+                    ),
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[100],
+                ),
+                obscureText: _obscureTextRepPass,
+              );
+            },
+          ),
+        ),
       ],
     );
   }
