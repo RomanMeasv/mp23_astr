2482b416091dd4f84a97cb29bde006ed2de52cc9
Sergio M <serg0971@easv365.dk>
2023-05-15 13:04:34 +0200

    Is working, ayayay

--- lib/app/modules/user_module/widgets/register_widget.dart
+++ lib/app/modules/user_module/widgets/register_widget.dart
@@ -10,9 +10,9 @@ class RegisterWidget extends StatefulWidget {
 
 class _RegisterWidgetState extends State<RegisterWidget> {
   final _validationFormKey = GlobalKey<FormState>();
-  final TextEditingController _nameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _repeatPasswordController = TextEditingController();
   bool _obscureTextRepPass = true;
   bool _obscureTextPass = true;
   final UserController controller = Get.find();
@@ -30,37 +30,7 @@ class _RegisterWidgetState extends State<RegisterWidget> {
               style: Theme.of(context).textTheme.titleLarge?.copyWith(
                     color: Theme.of(context).colorScheme.primary,
                   )),
-          const Padding(padding: EdgeInsets.only(top: 30)),
-
-          Container(
-            margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-            child: TextFormField(
-              controller: _nameController,
-              decoration: InputDecoration(
-                labelText: 'Username',
-                hintText: 'example@gmail.com',
-                prefixIcon: const Icon(Icons.email),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(5),
-                  borderSide: BorderSide(
-                    color: Theme.of(context).colorScheme.primary,
-                    width: 2.0,
-                  ),
-                ),
-                filled: true,
-                fillColor: Colors.grey[100],
-              ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter an email';
-                }
-                return null;
-              },
-            ),
-          ),
+          const Padding(padding: EdgeInsets.only(top: 70)),
           Container(
             margin: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
             child: TextFormField(
@@ -83,8 +53,8 @@ class _RegisterWidgetState extends State<RegisterWidget> {
                 fillColor: Colors.grey[100],
               ),
               validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter an email';
+                if (value == null || value.isEmpty || !value.isEmail) {
+                  return 'Please enter a valid email';
                 }
                 return null;
               },
@@ -141,7 +111,7 @@ class _RegisterWidgetState extends State<RegisterWidget> {
             child: StatefulBuilder(
               builder: (BuildContext context, StateSetter setState) {
                 return TextFormField(
-                  controller: _passwordController,
+                  controller: _repeatPasswordController,
                   decoration: InputDecoration(
                     labelText: 'Repeat password',
                     hintText: '••••••••',
@@ -173,10 +143,12 @@ class _RegisterWidgetState extends State<RegisterWidget> {
                   ),
                   obscureText: _obscureTextRepPass,
                   validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter an email';
+                    if (value == null || value.isEmpty || value.length < 6) {
+                      return "Please repeat the previously introduced password";
                     }
-                    // Add additional email validation logic if needed
+                    if(value != _passwordController.value.text){
+                      return "Password doesn't match";
+                    }
                     return null;
                   },
                 );
@@ -204,6 +176,78 @@ class _RegisterWidgetState extends State<RegisterWidget> {
                     ),
                   ),
                   child: Text('Register',
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.onPrimary)),
+                ),
+              )),
+          Container(
+              margin: const EdgeInsets.only(bottom: 20, left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(child: Divider(color: Colors.grey[400], thickness: 1)),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child: Text("or"),
+                  ),
+                  Expanded(
+                      child: Divider(color: Colors.grey[400], thickness: 1)),
+                ],
+              )),
+          Container(
+              height: 60,
+              width: double.infinity,
+              margin: const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: () {
+                    //implement google log in
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueAccent,
+                    padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  child: Text('Use your google account',
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.onSecondary)),
+                ),
+              )),
+          Container(
+              margin: const EdgeInsets.only(bottom: 20, left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(child: Divider(color: Colors.grey[400], thickness: 1)),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child: Text("or"),
+                  ),
+                  Expanded(
+                      child: Divider(color: Colors.grey[400], thickness: 1)),
+                ],
+              )),
+          Container(
+              height: 60,
+              width: double.infinity,
+              margin: const EdgeInsets.only(bottom: 20, right: 20, left: 20),
+              child: SizedBox(
+                child: ElevatedButton(
+                  onPressed: () {
+                    controller.isLogging(true);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                  child: Text('Log in',
                       style: TextStyle(
                           color: Theme.of(context).colorScheme.onPrimary)),
                 ),
