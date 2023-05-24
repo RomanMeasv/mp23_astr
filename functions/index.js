3b6cc88ad3e6e22946954f6796cad83f4db70c55
Ádám Lőrincz <90683098+Ladam0203@users.noreply.github.com>
2023-05-24 10:54:53 +0200

    Made the Cloud Funcs nicer

--- functions/index.js
+++ functions/index.js
@@ -1,56 +1,51 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-var serviceAccount = require("./mp23-astr-firebase-adminsdk-3gl9w-984d3164ee.json");
+const serviceAccount = require("./mp23-astr-firebase-adminsdk-3gl9w-984d3164ee.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://mp23-astr-default-rtdb.europe-west1.firebasedatabase.app"
 });
 
-exports.createUserOnSignUp = functions.auth
-    .user()
-    .onCreate((user) => {
-        admin.firestore().collection("Users").doc(user.uid)
-        .set({
-            email: user.email,
-            shoppingListIds: [],
-        });
+exports.createUserOnSignUp = functions.auth.user().onCreate(async (user) => {
+  const userData = {
+    email: user.email,
+    shoppingListIds: []
+  };
+
+  const userDocRef = admin.firestore().collection("Users").doc(user.uid);
+  await userDocRef.set(userData);
 });
 
 exports.sendNotificationWhenNewItemIsAddedToAShoppingListOfTheUser = functions.firestore
-.document('ShoppingLists/{listId}/Items/{itemId}')
-.onCreate(async (snap, context) => {
-    // Get the name of the shopping list
+  .document('ShoppingLists/{listId}/Items/{itemId}')
+  .onCreate(async (snap, context) => {
     const listId = context.params.listId;
     const listSnapshot = await admin.firestore().collection('ShoppingLists').doc(listId).get();
     const listName = listSnapshot.data().name;
 
     const itemName = snap.data().text;
 
-    let titles = [
-        `📢 Item added!`,
-        `🎉 A new item joins the list!`,
-        `🛍️ Another one buies the stuff!`,
-        ]
+    const titles = [
+      `📢 Item added!`,
+      `🎉 A new item joins the list!`,
+      `🛍️ Another one buys the stuff!`
+    ];
 
-    // Construct the notification message that it includes the name of the shopping list, and what item was added (text)
     const payload = {
-        notification: {
-            title: titles[Math.floor(Math.random()*titles.length)],
-            body: `${itemName} was added to ${listName}`,
-        }
+      notification: {
+        title: titles[Math.floor(Math.random() * titles.length)],
+        body: `${itemName} was added to ${listName}`
+      }
     };
 
-    //Fetch the user that have the list id in their shoppingListIds list
-    const users = await admin.firestore().collection('Users').where('shoppingListIds', 'array-contains', listId).get();
+    const usersSnapshot = await admin.firestore().collection('Users').where('shoppingListIds', 'array-contains', listId).get();
 
-    // Get the tokens of the users, the tokens are stored as an array in the user document
-    let allFcmTokens = []; //TODO: The user's own token might be excluded, but it's easier to test
-    users.forEach(user => {
-        allFcmTokens = allFcmTokens.concat(user.data().fcmTokens);
+    const allFcmTokens = [];
+    usersSnapshot.forEach(user => {
+      allFcmTokens.push(...user.data().fcmTokens);
     });
 
-    // Send the notification to all tokens
     const response = await admin.messaging().sendToDevice(allFcmTokens, payload);
 
     //If a notification fails to send, remove the token from the user's fcmTokens array
@@ -63,7 +58,7 @@ exports.sendNotificationWhenNewItemIsAddedToAShoppingListOfTheUser = functions.f
                 console.log('Failure sending notification to', failed_token, resp.error);
                 // Fetch users who have this FCM token - it is generally assumed that there is only one user having this token, but it is possible that there are more, if e.g. 2 users share the same device
                 const failed_token_users = await admin.firestore().collection('Users').where('fcmTokens', 'array-contains', failed_token).get();
-    
+
                 // Remove the failed token from each user
                 failed_token_users.forEach(doc => {
                     doc.ref.update({
@@ -73,7 +68,4 @@ exports.sendNotificationWhenNewItemIsAddedToAShoppingListOfTheUser = functions.f
             }
         }
     }
-
-    return null;
 });
-
